4342c2ecf7b161b0720ed9b23718e581905e4d0c
Pinmanee_Vajrapatkul <pinmani.nam@gmail.com>
2025-10-21 18:31:23 +0700

    เพิ่ม field เพิ่มข้อมูล

--- Django_project/WebSupport/views.py
+++ Django_project/WebSupport/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
-
+from datetime import datetime
+import pytz
 
 # Create your views here.
 
 def WebSupport(request):
     return render(request,"home.html")
+
+def report(request):
+    thailand_tz = pytz.timezone("Asia/Bangkok")
+    current_time = datetime.now(thailand_tz).strftime("%H:%M:%S")
+    return render(request, "report.html", {"current_time": current_time})
+
+
